a72023488da06b72a27402ed9b2eca0d883befda
BogdanKhrul <bobb52654@gmail.com>
2024-09-19 16:12:11 +0200

    Fix

--- main.js
+++ main.js
@@ -1,23 +1,46 @@
 'use-strict';
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
-    type: 'line', // тип графика
+    type: 'line',
     data: {
-        labels: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн'], // метки оси X
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Violet', 'Orange'],
         datasets: [
             {
-                label: 'Цена',
-                data: [12, 19, 3, 5, 2, 3], // данные для графика
-                borderColor: 'rgba(75, 192, 192, 1)',
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderWidth: 1,
+                label: '# votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1.5,
             },
         ],
     },
     options: {
+        responsive: false,
+        maintainAspectRatio: false,
         scales: {
             y: {
-                beginAtZero: true,
+                grid: {
+                    color: 'rgb(100, 100, 100)',
+                },
+            },
+            x: {
+                grid: {
+                    color: 'rgb(100, 100, 100)',
+                },
             },
         },
     },
